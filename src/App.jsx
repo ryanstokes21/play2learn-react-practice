2c7df553e1d169225e90e7a2a398f1ae9e4eb355
Ryan Stokes <ryanstokescfbre@gmail.com>
2026-06-27 16:17:44 -0500

    Initial commit: Setup Vite React app

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <section>
+        <h1>
+          Play<span>2</span>Learn
+        </h1>
+        <div className="card">
+          <h2>Anagram Hunt</h2>
+          <p>
+            Do you like scrabble? Words with friends? Improve how fast you can
+            recognize anagrams in a word with this neat little game.
+          </p>
+          <div className="button-section">
+            <button>play</button>
+          </div>
+        </div>
+        <div className="card">
+          <h2>Anagram Hunt</h2>
+          <p>
+            Do you like scrabble? Words with friends? Improve how fast you can
+            recognize anagrams in a word with this neat little game.
+          </p>
+          <div className="button-section">
+            <button>play</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
