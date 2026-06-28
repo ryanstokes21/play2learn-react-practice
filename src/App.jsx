9bced4e4a77d0f39b9344e92c7376165672180aa
Ryan Stokes <ryanstokescfbre@gmail.com>
2026-06-28 17:13:09 -0500

    add useState for operation and number

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import StartScreen from './components/StartScreen';
@@ -7,12 +8,24 @@ import MathFactsGame from './components/MathFacts/MathFactsGame';
 import AnagramHuntGame from './components/AnagramHunt/AnagramHuntGame';
 
 function App() {
+  const [operation, setOperation] = useState('x');
+  const [maxNumber, setMaxNumber] = useState(0);
   return (
     <Routes>
       <Route path="/" element={<StartScreen />} />
       <Route path="/anagram-hunt" element={<AnagramHuntScreen />} />
       <Route path="/anagram-hunt-game" element={<AnagramHuntGame />} />
-      <Route path="/math-facts" element={<MathFactsScreen />} />
+      <Route
+        path="/math-facts"
+        element={
+          <MathFactsScreen
+            operation={operation}
+            setOperation={setOperation}
+            maxNumber={maxNumber}
+            setMaxNumber={setMaxNumber}
+          />
+        }
+      />
       <Route path="/math-facts-game" element={<MathFactsGame />} />
     </Routes>
   );
